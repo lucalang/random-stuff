601016456836ae1a6742c23d931ece326e2f71d0
Luca Lang <luca.lang@swisscom.com>
2026-08-21 15:47:04 +0200

    vercel deploy ready

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -6,6 +6,14 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(import.meta.dirname, './index.html'),
+        'easter-egg': path.resolve(import.meta.dirname, './easter-egg.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
